70aed9db654ea9f35d0a5a34a0b64e3a9cac204b
unknown <allard.lucas.97@gmail.com>
2025-01-04 14:42:57 +0100

    fix vercel deploy

--- src/app/api/admin/[[...nextadmin]]/route.ts
+++ src/app/api/admin/[[...nextadmin]]/route.ts
@@ -2,20 +2,12 @@ import {createHandler} from "@premieroctet/next-admin/appHandler";
 import schema from "../../../../../prisma/json-schema/json-schema.json";
 import {prisma} from "../../../../../prisma/lib/prisma";
 import {options} from "@/lib/nextAdmin/options";
-import {isGranted} from "@/lib/authUtils";
-import {NextResponse} from "next/server";
-import {Role} from "@prisma/client";
 
 const { run } = createHandler({
     apiBasePath: "/api/admin",
     prisma,
     schema,
     options,
-    onRequest: async (req) => {
-        if (!(await isGranted(req, [Role.ADMIN, Role.MODERATOR]))) {
-            return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
-        }
-    }
 });
 
 export { run as DELETE, run as GET, run as POST };
