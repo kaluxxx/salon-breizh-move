93c5681483dbc24d7557d04c8fa30bf340ac224f
unknown <allard.lucas.97@gmail.com>
2025-01-27 12:07:48 +0100

    fix text-color

--- src/app/overview/_component/KeyFiguresSection.tsx
+++ src/app/overview/_component/KeyFiguresSection.tsx
@@ -18,7 +18,7 @@ export default function KeyFiguresSection() {
     });
 
     return (
-        <section ref={ref} className="py-20 bg-primary text-primary-foreground">
+        <section ref={ref} className="py-20 bg-primary text-white">
             <div className="container mx-auto px-4">
                 <motion.h2
                     initial={{ opacity: 0, y: -20 }}
